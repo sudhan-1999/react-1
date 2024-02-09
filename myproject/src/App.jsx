2b45dee45dc6c5c9a923205f03d00e745ddefacd
sudhan-1999 <jsudhan68@gmail.com>
2024-02-09 18:28:02 +0530

    add a new file

---
+++ myproject/src/App.jsx
@@ -0,0 +1,68 @@
+/*import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'*/
+import './App.css'
+
+function App() {
+  
+  return (
+    <div className='back'>
+    <div class="row">
+  <div class="col-sm-4">
+    <div class="card">
+      <div class="card-body">
+        <h4>Free</h4>
+        <h5 class="card-title">$0/month</h5>
+        <h6><i class="fa-solid fa-check"></i> Single User</h6>
+        <h6><i class="fa-solid fa-check"></i> 50GB Storage</h6>
+        <h6><i class="fa-solid fa-check"></i> Unlimited Public Project</h6>
+        <h6><i class="fa-solid fa-check"></i> Community Access</h6>
+        <h6 class="txt"><i class="fa-solid fa-xmark"></i> Unlimited Private Project</h6>
+        <h6 class="txt"><i class="fa-solid fa-xmark"></i> Dedicated Phone support</h6>
+        <h6 class="txt"><i class="fa-solid fa-xmark"></i> Free Subdomain</h6>
+        <h6 class="txt"><i class="fa-solid fa-xmark"></i> Monthly Status Reports</h6>
+        <a href="#" class="btn btn-primary">B U T T O N</a>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-4">
+    <div class="card">
+      <div class="card-body">
+      <h4>Plus</h4>
+        <h5 class="card-title">$9/month</h5>
+        <h6><i class="fa-solid fa-check"></i> Single User</h6>
+        <h6><i class="fa-solid fa-check"></i> 50GB Storage</h6>
+        <h6><i class="fa-solid fa-check"></i> Unlimited Public Project</h6>
+        <h6><i class="fa-solid fa-check"></i> Community Access</h6>
+        <h6><i class="fa-solid fa-check"></i> Unlimited Private Project</h6>
+        <h6><i class="fa-solid fa-check"></i> Dedicated Phone support</h6>
+        <h6><i class="fa-solid fa-check"></i> Free Subdomain</h6>
+        <h6 class="txt"><i class="fa-solid fa-xmark"></i> Monthly Status Reports</h6>
+        <a href="#" class="btn btn-primary">B U T T O N</a>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-4">
+    <div class="card">
+      <div class="card-body">
+      <h4>Pro</h4>
+        <h5 class="card-title">$49/month</h5>
+        <h6><i class="fa-solid fa-check"></i> Single User</h6>
+        <h6><i class="fa-solid fa-check"></i> 50GB Storage</h6>
+        <h6><i class="fa-solid fa-check"></i> Unlimited Public Project</h6>
+        <h6><i class="fa-solid fa-check"></i> Community Access</h6>
+        <h6><i class="fa-solid fa-check"></i> Unlimited Private Project</h6>
+        <h6><i class="fa-solid fa-check"></i> Dedicated Phone support</h6>
+        <h6><i class="fa-solid fa-check"></i> Free Subdomain</h6>
+        <h6><i class="fa-solid fa-check"></i> Monthly Status Reports</h6>
+        <a href="#" class="btn btn-primary">B U T T O N</a>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+ )
+ }
+ 
+
+export default App
